df14002fa5e001da5f2ff71ccd57453a8225e2da
pirzer <pirzer1@yahoo.com>
2023-03-05 22:36:46 +0000

    error AuthenticationError at checkout You did not provide an API key

--- checkout/views.py
+++ checkout/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 
-
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
@@ -10,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -18,13 +20,23 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MiKbtAICwh7ePkuai2vv8ECsc57f8asCIODptw7C5FUlYZ00TWRnE5JFTHOp45VHyVzdFGxDSWnw3DTGwyG1RIx00MyHiDRBF',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
